4f59610b07254459d405097b417b0d7316060c99
Developer-Nasim <unique0coder@gmail.com>
2021-06-05 20:59:38 +0600

    upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,7 +100,19 @@
 
 
 
-
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 50) {
+          $(".scrollToTop:hidden").stop(true, true).fadeIn();
+      } else {
+          $(".scrollToTop").stop(true, true).fadeOut();
+      }
+  });
+  $(function () {
+      $(".scroll").click(function () {
+          $("html,body").animate({ scrollTop: $(".top").offset().top }, "1000");
+          return false;
+      });
+  });
 
 
 
